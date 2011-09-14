b433ac9a26f3090f13192ecf56618221a0682047
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-09-14 00:43:14 -0500

    Change settings for Rails 3.1.0

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tweetranslate::Application.config.secret_token = 'b51306c8ce446fbe6216356fbd7585e10ce37038d3d5a1010d3cb4735c6c7b4f73ba53504da3dcff0760b083c879e7f6f925635c4f9d2086db92b50cc26eb2fd'
+Tweetranslate::Application.config.secret_token = 'b8c9f80c3b90ca35af37d99094b4dac632114adbd58e3c64a0f02922f0834da080ce0c9f333cd8e04d947183e42e5ff46715cc94f7ca555cca383198ae9be935'
