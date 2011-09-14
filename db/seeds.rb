a2d569c7432aeaa6f9bc71784d5d7fcac1f1f826
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-09-14 01:21:05 -0500

    Remove unused data from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-user = User.create(:original => "anteseraotro", :original_uid => "18735167",
-                   :translated => "guilleiguaran", :translated_uid => "252932427",
-                   :lang_from => 'es', :lang_to => 'en',
-                   :token => "252932427-3ppZwS3RdAOieWtBOb7QrGhcS645xCn5T8Hl9w0t",
-                   :secret => "mQDBJz6P4duLaIzO1m9xBJBSmoP75qKrMtYWZPBts")
-
-user.add_to_watching
